c3b25e13f5efbd9d4afb170879a2f018422ad616
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-02-10 23:27:45 +0700

    Сделан метод Якобы, не получается вернуть значения X

--- ComputationalAlgorithms2.cpp
+++ ComputationalAlgorithms2.cpp
@@ -5,6 +5,11 @@
 
 void MatrixOutput(std::vector<std::vector<double>> Matrix, int Dimension, std::ostream& Stream);
 void VectorOutput(std::vector<double>Vector, int Dimension, std::ostream& Stream);
+int JacobisMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeTermsColumn,
+                  std::vector<double> X, int Dimension, double Accuracy);
+int SeidelsMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeTermsColumn,
+                   std::vector<double> X, int Dimension, double Accuracy);
+double SumOfRowWithoutOneElement(std::vector<std::vector<double>> Matrix, std::vector<double> X, int Dimension, int IndexOfRow);
 
 int main()
 {
@@ -48,10 +53,10 @@ int main()
 
     std::string AccuracyFileName = "Accuracy.txt";
     std::ifstream InputAccuracyFile(AccuracyFileName);
-    double Accurancy = 0.0;
+    double Accuracy = 0.0;
     if (InputAccuracyFile.is_open())
     {
-        InputAccuracyFile >> Accurancy;
+        InputAccuracyFile >> Accuracy;
         InputAccuracyFile.close();
     }
     else
@@ -66,9 +71,16 @@ int main()
         OutputFile << std::endl << "Столбец свободных членов: " << std::endl;
         VectorOutput(FreeTermsColumn, Dimension, OutputFile);
         OutputFile << std::endl << "Точность: " << std::endl;
-        OutputFile << Accurancy << std::endl;
+        OutputFile << Accuracy << std::endl;
+        OutputFile << std::endl << "Количество итераций: " << std::endl;
+        std::vector<double> X(Dimension);
+        OutputFile << JacobisMethod(Matrix, FreeTermsColumn, X, Dimension, Accuracy) << std::endl;
+        OutputFile << std::endl << "Решения СЛАУ: " << std::endl;
+        VectorOutput(X, Dimension, OutputFile);
         OutputFile.close();
     }
+    else
+        std::cout << "Ошибка! Не удалось открыть файл для вывода!" << std::endl;
     return 0;
 }
 
@@ -90,4 +102,49 @@ void VectorOutput(std::vector<double>Vector, int Dimension, std::ostream& Stream
     {
         Stream << Vector[i] << std::endl;
     }
+}
+
+double SumOfRowWithoutOneElement(std::vector<std::vector<double>> Matrix, std::vector<double> X, int Dimension, int IndexOfRow)
+{
+    double Sum = 0;
+    for(int i = 0; i < Dimension; i++)
+        if(IndexOfRow != i)
+            Sum += Matrix[IndexOfRow][i] * X[i];
+    return Sum;
+}
+
+int JacobisMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeTermsColumn,
+                              std::vector<double> X, int Dimension, double Accuracy)
+{
+    bool DiagonalFlag = false;
+    for (int i = 0; i < Dimension; i++)
+    {
+        if (Matrix[i][i] == 0)
+        {
+            DiagonalFlag = true;
+            break;
+        }
+    }
+    //В вектор X вносятся стартовые решения СЛАУ
+    for(int i = 0; i < Dimension; i++)
+    {
+        X[i] = 0.0;
+    }
+
+    int k = 0;
+    if(!DiagonalFlag)
+    {
+        for (k = 0; k > -1; k++)
+        {
+            std::vector<double> XPrevious = X;
+            for (int i = 0; i < Dimension; i++)
+            {
+                X[i] = (FreeTermsColumn[i] - SumOfRowWithoutOneElement(Matrix, X, Dimension, i))/Matrix[i][i];
+                std::cout << X[i] << std::endl;
+            }
+            if (fabs(X[0] - XPrevious[0]) < Accuracy)
+                break;
+        }
+    }
+    return k;
 }
