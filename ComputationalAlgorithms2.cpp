5c47a03d749dff984083c10cda8f77547f6aca02
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-02-11 15:44:42 +0700

    Сделан метод Зейделя

--- ComputationalAlgorithms2.cpp
+++ ComputationalAlgorithms2.cpp
@@ -6,10 +6,13 @@
 void MatrixOutput(std::vector<std::vector<double>> Matrix, int Dimension, std::ostream& Stream);
 void VectorOutput(std::vector<double>Vector, int Dimension, std::ostream& Stream);
 int JacobisMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeTermsColumn,
-                  std::vector<double> X, int Dimension, double Accuracy);
+                  std::vector<double> *const X, int Dimension, double Accuracy);
 int SeidelsMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeTermsColumn,
-                   std::vector<double> X, int Dimension, double Accuracy);
+                   std::vector<double> *const X, int Dimension, double Accuracy);
 double SumOfRowWithoutOneElement(std::vector<std::vector<double>> Matrix, std::vector<double> X, int Dimension, int IndexOfRow);
+bool CheckDiagonal(std::vector<std::vector<double>> Matrix, int Dimension);
+bool CheckAccuracy(std::vector<double> X, std::vector<double> XPrevious, int Dimension, double Accuracy);
+void SetStartingSolution(std::vector<double>* const X, int Dimension);
 
 int main()
 {
@@ -26,7 +29,7 @@ int main()
     else
         std::cout << "Ошибка! Не удалось открыть файл для ввода размерности матрицы!" << std::endl;
 
-    std::string MatrixFileName = "Matrix.txt";
+    std::string MatrixFileName = "A0.txt";
     std::ifstream InputMatrixFile(MatrixFileName);
     std::vector<std::vector<double>> Matrix(Dimension, std::vector<double>(Dimension));
     if (InputMatrixFile.is_open())
@@ -39,7 +42,7 @@ int main()
     else
         std::cout << "Ошибка! Не удалось открыть файл для ввода матрицы!" << std::endl;
 
-    std::string FreeTermsColumnFileName = "FreeTermsColumn.txt";
+    std::string FreeTermsColumnFileName = "B0.txt";
     std::ifstream InputFreeTermsColumnFile(FreeTermsColumnFileName);
     std::vector<double> FreeTermsColumn(Dimension);
     if (InputFreeTermsColumnFile.is_open())
@@ -72,10 +75,14 @@ int main()
         VectorOutput(FreeTermsColumn, Dimension, OutputFile);
         OutputFile << std::endl << "Точность: " << std::endl;
         OutputFile << Accuracy << std::endl;
-        OutputFile << std::endl << "Количество итераций: " << std::endl;
+        OutputFile << std::endl << "Количество итераций метода Якоби: " << std::endl;
         std::vector<double> X(Dimension);
-        OutputFile << JacobisMethod(Matrix, FreeTermsColumn, X, Dimension, Accuracy) << std::endl;
-        OutputFile << std::endl << "Решения СЛАУ: " << std::endl;
+        OutputFile << JacobisMethod(Matrix, FreeTermsColumn, &X, Dimension, Accuracy) << std::endl;
+        OutputFile << std::endl << "Решения СЛАУ метода Якоби: " << std::endl;
+        VectorOutput(X, Dimension, OutputFile);
+        OutputFile << std::endl << "Количество итераций метода Зейделя: " << std::endl;
+        OutputFile << SeidelsMethod(Matrix, FreeTermsColumn, &X, Dimension, Accuracy) << std::endl;
+        OutputFile << std::endl << "Решения СЛАУ метода Зейделя: " << std::endl;
         VectorOutput(X, Dimension, OutputFile);
         OutputFile.close();
     }
@@ -114,37 +121,68 @@ double SumOfRowWithoutOneElement(std::vector<std::vector<double>> Matrix, std::v
 }
 
 int JacobisMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeTermsColumn,
-                              std::vector<double> X, int Dimension, double Accuracy)
+                              std::vector<double> *const X, int Dimension, double Accuracy)
 {
-    bool DiagonalFlag = false;
-    for (int i = 0; i < Dimension; i++)
-    {
-        if (Matrix[i][i] == 0)
-        {
-            DiagonalFlag = true;
-            break;
-        }
-    }
-    //В вектор X вносятся стартовые решения СЛАУ
-    for(int i = 0; i < Dimension; i++)
-    {
-        X[i] = 0.0;
-    }
-
+    SetStartingSolution(X, Dimension);
     int k = 0;
-    if(!DiagonalFlag)
+    if(CheckDiagonal(Matrix, Dimension))
     {
         for(k = 0; k > -1; k++)
         {
-            std::vector<double> XPrevious = X;
+            std::vector<double> XPrevious = *X;
+            VectorOutput(XPrevious, Dimension, std::cout);
             for (int i = 0; i < Dimension; i++)
-            {
-                X[i] = (FreeTermsColumn[i] - SumOfRowWithoutOneElement(Matrix, X, Dimension, i))/Matrix[i][i];
-                std::cout << X[i] << std::endl;
-            }
-            if (fabs(X[0] - XPrevious[0]) < Accuracy)
+                (*X)[i] = (FreeTermsColumn[i] - SumOfRowWithoutOneElement(Matrix, XPrevious, Dimension, i)) / Matrix[i][i];
+            if (CheckAccuracy(*X, XPrevious, Dimension, Accuracy))
                 break;
         }
     }
     return k;
+}
+
+int SeidelsMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeTermsColumn,
+                  std::vector<double>* const X, int Dimension, double Accuracy)
+{
+    SetStartingSolution(X, Dimension);
+    int k = 0;
+    if (CheckDiagonal(Matrix, Dimension))
+    {
+        for (k = 0; k > -1; k++)
+        {
+            std::vector<double> XPrevious = *X;
+            for (int i = 0; i < Dimension; i++)
+                (*X)[i] = (FreeTermsColumn[i] - SumOfRowWithoutOneElement(Matrix, *X, Dimension, i)) / Matrix[i][i];
+            if (CheckAccuracy(*X, XPrevious, Dimension, Accuracy))
+                break;
+        }
+    }
+    return k;
+}
+
+bool CheckDiagonal(std::vector<std::vector<double>> Matrix, int Dimension)
+{
+    for (int i = 0; i < Dimension; i++)
+        if (Matrix[i][i] == 0)
+            return false;
+    return true;
+}
+
+bool CheckAccuracy(std::vector<double> X, std::vector<double> XPrevious, int Dimension, double Accuracy)
+{
+    double max = fabs(X[0] - XPrevious[0]);
+    for (int i = 1; i < Dimension; i++)
+        if (fabs(X[i] - XPrevious[i]) > max)
+            max = fabs(X[i] - XPrevious[i]);
+    if (max < Accuracy)
+        return true;
+    else
+        return false;
+}
+
+void SetStartingSolution(std::vector<double>* const X, int Dimension)
+{
+    for (int i = 0; i < Dimension; i++)
+    {
+        (*X)[i] = 0.0;
+    }
 }
